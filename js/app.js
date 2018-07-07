56d8d684c0ed3fb1b6e76e42737635b21d3969f3
Amy Silva <amy_silva@me.com>
2018-07-07 13:09:54 -0700

    added left handed direction controls - AWDS

--- js/app.js
+++ js/app.js
@@ -8,10 +8,14 @@ const allEnemies = [...Array(3)].map((_,i)=> new Enemy(0,i+1));
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: 'left',     // left arrow
+        38: 'up',       // up arrow
+        39: 'right',    // right arrow
+        40: 'down',     // down arrow
+        65: 'left',     // A
+        87: 'up',       // W
+        68: 'right',    // D
+        83: 'down'      // S
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
