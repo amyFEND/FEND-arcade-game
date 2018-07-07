4540abfcffb72ff1b309491e395a235ad3b8273e
Amy Silva <amy_silva@me.com>
2018-07-07 10:44:49 -0700

    Created instance of Player image

--- js/classes.js
+++ js/classes.js
@@ -9,3 +9,10 @@ class Entity {
     ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83); //101&83 is hard coding the off-set
   }
 }
+
+class Player extends Entity {
+  constructor () {
+    super ();
+    this.sprite += 'char-princess-girl.png';
+  }
+}
