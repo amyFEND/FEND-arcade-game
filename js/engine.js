472f7cfe37f9d3a7be32f6a1bd4d5897a7ca5eee
Amy Silva <amy_silva@me.com>
2018-07-07 22:53:59 -0700

    Updating title to match new asthetic

--- js/engine.js
+++ js/engine.js
@@ -7,7 +7,7 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
+    canvas.width = 515;
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
